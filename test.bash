5889056eb50de94d7b7ccb8ab9f10866c0608b54
Rion Miuar <rionmiura39@gmail.com>
2022-10-27 12:31:22 +0900

    test.bash

--- test.bash
+++ test.bash
@@ -1,5 +1,18 @@
 #!/bin/bash
+# SPDX - FilecopyrightText: 2022 Rion Miur
+# SPDX - License-Identifier: BSD-3-Clause
+
+ng (){
+	echo NG at Line $1
+	res=1
+}
+
+res=0
+
 
 out=$(seq 5 | ./plus)
 
-[ "${out}" = 15 ]
+[ "${out}" = 15 ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK
+exit $res
